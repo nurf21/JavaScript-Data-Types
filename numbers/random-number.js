343499417cf5c7ed6afaa76119dcc751dde39635
nurf21 <nurfauzangiri@gmail.com>
2025-06-03 14:44:40 +0700

    Added script to generate a random number

---
+++ numbers/random-number.js
@@ -0,0 +1,10 @@
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+console.log(random(1, 5));
+console.log(random(1, 5));
+console.log(random(1, 5));
+
+console.log(random(-3, -1));
+console.log(random(10, 100)); 
