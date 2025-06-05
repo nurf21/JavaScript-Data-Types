7fe73f82bd90d8c1a396679268880350849ad600
nurf21 <nurfauzangiri@gmail.com>
2025-06-05 16:51:33 +0700

    Added function to check for spam

--- strings/strings.js
+++ strings/strings.js
@@ -6,4 +6,16 @@ console.log(ucFirst("john"));
 console.log(ucFirst("a"));
 console.log(ucFirst(""));
 console.log(ucFirst("hello"));
-console.log(ucFirst("123"));   
+console.log(ucFirst("123"));
+
+function checkSpam(str) {
+  const lowerStr = str.toLowerCase();
+  return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+}
+
+console.log(checkSpam('buy ViAgRA now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam("innocent rabbit"));
+console.log(checkSpam('VIAGRA'));
+console.log(checkSpam('xXx'));
+console.log(checkSpam(''));                
