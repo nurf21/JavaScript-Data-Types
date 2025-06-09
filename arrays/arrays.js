70f03427ccc9ae2f3d08d851ac73192b8d93cd5c
nurf21 <nurfauzangiri@gmail.com>
2025-06-09 13:17:17 +0700

    Added getMaxSubSum function and test cases to arrays.js

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -19,3 +19,24 @@ console.log(styles);
 // Step 5: Prepend "Rap" and "Reggae"
 styles.unshift("Rap", "Reggae");
 console.log(styles);
+
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let currentSum = 0;
+
+  for (let num of arr) {
+    currentSum += num;
+    if (currentSum > maxSum) maxSum = currentSum;
+    if (currentSum < 0) currentSum = 0;
+  }
+
+  return maxSum;
+}
+
+console.log(getMaxSubSum([-1, 2, 3, -9]));
+console.log(getMaxSubSum([2, -1, 2, 3, -9]));
+console.log(getMaxSubSum([-1, 2, 3, -9, 11]));
+console.log(getMaxSubSum([-2, -1, 1, 2]));
+console.log(getMaxSubSum([100, -9, 2, -3, 5]));
+console.log(getMaxSubSum([1, 2, 3]));
+console.log(getMaxSubSum([-1, -2, -3]));
