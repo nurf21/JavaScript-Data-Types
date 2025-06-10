ac22b9043ea6f1d4fc1178eff36f5d83407ea7ec
nurf21 <nurfauzangiri@gmail.com>
2025-06-10 11:03:54 +0700

    Added groupById function with example usage

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -277,4 +277,24 @@ let strings = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 
-console.log( unique(strings) );
+console.log(unique(strings));
+
+// Create keyed object from array
+// ==================================================
+
+function groupById(arr) {
+  return arr.reduce((obj, user) => {
+    obj[user.id] = user;
+    return obj;
+  }, {});
+}
+
+let users3 = [
+  { id: 'john', name: "John Smith", age: 20 },
+  { id: 'ann', name: "Ann Smith", age: 24 },
+  { id: 'pete', name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users3);
+
+console.log(usersById)
